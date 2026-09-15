ce4b6b92bbf02ebce7e4b77dc969deec3ae1ebc0
Casey <34421290+MoreOverMedia@users.noreply.github.com>
2026-09-15 10:40:20 -0500

    Refactor presenceUpdate event handling logic
    Condensed code

--- bot.js
+++ bot.js
@@ -7,47 +7,21 @@ client.on('ready', () => {
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
   const user = newPresence.user;
-
-    // Check if the user is the one we want to ping
     if (user.id === '256213318060998666') {
-
-//old status online
-
-        if (oldPresence.status != 'online' && newPresence.status === 'online') {
-            // Get the channel to send the message in
+      if (oldPresence.status != 'online' && newPresence.status === 'online') {
             const channel = client.channels.cache.get('913633377448509481');
-        
-            // Send the message
             channel.send(`https://imgur.com/a/wlmieyo`);
         }
-
-//old status idle
-
         else if (oldPresence.status != 'idle' && newPresence.status === 'idle') {
-            // Get the channel to send the message in
             const channel = client.channels.cache.get('913633377448509481');
-        
-            // Send the message
             channel.send(`https://tenor.com/view/stormidle-gif-27216955`);
         }
-
-//old status dnd
-
         else if (oldPresence.status != 'dnd' && newPresence.status === 'dnd') {
-            // Get the channel to send the message in
             const channel = client.channels.cache.get('913633377448509481');
-        
-            // Send the message
             channel.send(`https://imgur.com/a/Bp6FLPA`);
         }
-
-//old status online
-
         else if (oldPresence.status != 'offline' && newPresence.status === 'offline') {
-            // Get the channel to send the message in
             const channel = client.channels.cache.get('913633377448509481');
-        
-            // Send the message
             channel.send(`https://tenor.com/view/stormwavz-omniversal-discord-gif-22696441`);
         }
     }
